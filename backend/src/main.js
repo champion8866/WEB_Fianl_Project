f6413adab472bedec491211b7f91388b23967fbb
champion8866 <champion8866@gmail.com>
2021-06-29 20:33:25 +0800

    updated

--- backend/src/main.js
+++ backend/src/main.js
@@ -8,13 +8,15 @@ import path from 'path';
 require('dotenv-defaults').config();
 
 const app = express();
-const buildPath = path.join(__dirname, '..', 'build');
+const buildPath = path.join(__dirname, '../../build');
 app.use(express.static(buildPath));
 app.use(cors());
 app.use(express.json());
 app.use('/', routes);
 
 mongo.connect();
+
+console.log(buildPath);
 
 const server = app.listen(process.env.PORT || 5000, function () {
   console.log('Listening on port ' + server.address().port);
