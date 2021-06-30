e6cf007ea3ff0dc02a42f025c8cded4d4d5dc327
champion8866 <champion8866@gmail.com>
2021-06-30 15:50:38 +0800

    add wakeupdyno

--- backend/src/main.js
+++ backend/src/main.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes';
 import path from 'path';
+import wakeUpDyno from './routes/wakeUpDyno'
 
 // gotta load in MONGO_URL before `mongo.connect()`
 require('dotenv-defaults').config();
@@ -19,5 +20,7 @@ mongo.connect();
 console.log(buildPath);
 
 const server = app.listen(process.env.PORT || 5000, function () {
+  const DYNO_URL = "https://meetup-tw.herokuapp.com/";
+  wakeUpDyno(DYNO_URL);
   console.log('Listening on port ' + server.address().port);
 });
